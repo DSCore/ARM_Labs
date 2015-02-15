ff2a7a5cbfa10aa48a6536e3b9ff4ab4a2f38c3f
armdev <armdev@armdev.(none)>
2015-02-14 22:15:35 -0500

    Working version of lab 4, week 2

---
+++ lab4-fsm-template-2/fsm.h
@@ -0,0 +1,42 @@
+#include <stdint.h>
+
+typedef enum {
+	STATE_RESET = 0,
+	STATE_STARTTRAFFIC,
+	STATE_TRAFFICNOWALK,
+	STATE_TRAFFICFLOW,
+	STATE_YLWLIGHT,
+	STATE_REDLIGHT,
+	STATE_CLOSEGATE,
+	STATE_GATECLOSED,
+	STATE_MAINTON,
+	STATE_MAINTOFF,
+	STATE_GATEOPEN,
+	STATE_WALK
+} state_t;
+
+/* Initialize the finite state machine */
+void fsm_init(void);
+
+/* Acquire a lock on the fsm before accessing/altering state */
+#define FSM_LOCK_FAILED   (0)
+#define FSM_LOCK_ACQUIRED (1)
+uint8_t fsm_lock(void);
+void fsm_unlock(void);
+
+uint8_t getPerson(void);
+void setPerson(uint8_t);
+uint8_t getArriving(void);
+void setArriving(uint8_t);
+uint8_t getHold(void);
+void setHold(uint8_t);
+uint8_t getClear(void);
+void setClear(uint8_t);
+uint32_t getTimer(void);
+void setTimer(uint32_t val);
+
+/* Query FSM state - only do this when you have acquired the lock! */
+state_t fsm_get_state(void);
+
+/* Alter FSM state - only do this when you have acquired the lock! */
+void fsm_set_state(state_t new_state);
---
+++ lab4-fsm-template-2/main.d
@@ -0,0 +1,18 @@
+main.o: main.c fsm.h stdint.h USART2.h userbutton.h systick.h stm32f4xx.h \
+ TIMER.h LED.h
+
+fsm.h:
+
+stdint.h:
+
+USART2.h:
+
+userbutton.h:
+
+systick.h:
+
+stm32f4xx.h:
+
+TIMER.h:
+
+LED.h:
